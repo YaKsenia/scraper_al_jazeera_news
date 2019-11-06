576ffc6f9104c4c588c6db5e9be6b87f36b538b7
Ksenia Yakovleva <xenia.iakovleva@gmail.com>
2019-11-06 11:15:14 +0100

    added explicit comments to the code, removed unnecessary parts

--- my_jazeera.py
+++ my_jazeera.py
@@ -1,3 +1,19 @@
+#this script scrapes all the links of the news articles from the website of Al Jazeera (search)
+
+#How to use it?
+
+#Go to https://www.aljazeera.com/Search/ and choose the keywords you want to search for
+
+#Copy the link with your keywords (e.g. we were looking for "Russia syria war" and the link was this:
+#'https://www.aljazeera.com/Search/?q=russia%20syria%20war')
+
+#If you want to scrape the news of a particular time period, unfortunately Al Jazeera doesn't
+#have filter by date. So what we do - manually check the numbers of pages with the dates 
+#we want to scrape and use them in the "for" loop (e. g. we needed only articles from pages 
+#81 - 91. The scraper clicks on the pages from the first one until it gets the page numbers
+#which you want and starts scraping only them)
+
+
 from bs4 import BeautifulSoup, SoupStrainer
 import urllib.request as urllib2
 import re
@@ -16,55 +32,73 @@ from selenium.common.exceptions import TimeoutException
 
 
 driver = webdriver.Firefox()
+#open the link
 driver.get('https://www.aljazeera.com/Search/?q=russia%20syria%20war')
 driver.maximize_window()
-#WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.LINK_TEXT, "I Accept")))
-#WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "enstAccept")))
 
-
+#close the banner on the website
 driver.find_element_by_id('ensCloseBanner').click()
-#driver.find_element_by_link_text('I Accept').click()
-#driver.find_element_by_class_name('ensButtons').click()
 
-#element = driver.find_element_by_class_name('ensButtons')
-#driver.execute_script("arguments[0].click();", element)
 
-while True:
-	try:
-		#print('yeeees')
-		number = 1
-		links = []
-		for i in range (0, 120):
-				#driver.find_element_by_class_name("page-link").click()
-				driver.find_element_by_link_text(str(number)).click()
-				print ('clicked page',  number)
-				driver.implicitly_wait(10)
-				#articles = html.findall('div', {'class': "row topics-sec-item"})
-				if number > 90:
-					html = driver.page_source
-					html = BeautifulSoup(html, "lxml")
-					#articles = html.find('h2', {'class': 'row topics-sec-item'})
-					articles = html.find_all('div', {'class': 'col-sm-7 topics-sec-item-cont'})
-					for article in articles:
-						ankor_list = article.findChildren('a')
-						for ankor in ankor_list:
-						    url = ankor.get('href')
-						    url = 'https://www.aljazeera.com' + url
-						    #if url != 'https://www.aljazeera.com/indepth/opinion' and url != 'https://www.aljazeera.com/topics/regions/middleeast.html' and url != 'https://www.aljazeera.com/topics/country/syria.html' and url != 'https://www.aljazeera.com/topics/regions/us-canada.html' and url != 'https://www.aljazeera.com/topics/regions/europe.html'
-						    if 'news' in url or 'opinion' in url:
-							    links.append(url)
-							    print(url)
-				#print(links)
+try:
+	#this is a page number, it will be updated after every time the scraper clicks on a page
+	number = 1
+	links = []
+	for i in range (0, 91):
+
+			#find the button with page number and click
+
+			driver.find_element_by_link_text(str(number)).click()
+			print ('clicked page',  number)
+
+			#wait 10 sec so the website will not think we are robots
+			driver.implicitly_wait(10)
+
+			#here you should specify the range of pages you want to scrape (you can start with 1)
+			if number >= 81 and number <= 91:
+				html = driver.page_source
+				html = BeautifulSoup(html, "lxml")
+				articles = html.find_all('div', {'class': 'col-sm-7 topics-sec-item-cont'})
+
+				for article in articles:
+
+					#find all links on the page
+					ankor_list = article.findChildren('a')
+
+					for ankor in ankor_list:
+
+					    url = ankor.get('href')
+					    url = 'https://www.aljazeera.com' + url
+					    #save url only if it is a news or opinion article (so we get read of social media links, advertisements etc.)
+					    if 'news' in url or 'opinion' in url:
+						    links.append(url)
+
+
+				#go to the next page
 				number = number + 1
+			#print(links)
 
-		print(links)
-		links = pd.DataFrame({'links' : links })
-		links = links.drop_duplicates(subset='links', keep='last', inplace=False)
-		links.to_csv('al_jazeera_links_final.csv')
+			#to speed up the process, before we achieve the number of page that we want to start with, 
+			#click on every 4th page instead of every 1st
+			
+			elif number < 81:
+				number = number + 4
 
-	except NoSuchElementException:
-		break
+			else:
+				
+				#break after we scrape the last page
+				break
 
-	finally:
-		#driver.quit()
-		print('bye')
+	#print(links)
+	
+	#save the links as Pandas dataframe, drop duplicates and save as a csv-file
+	links = pd.DataFrame({'links' : links })
+	links = links.drop_duplicates(subset='links', keep='last', inplace=False)
+	links.to_csv('al_jazeera_links_check.csv')
+
+except Exception as e:
+	print(e)
+
+finally:
+	driver.quit()
+	print('Finished.')
