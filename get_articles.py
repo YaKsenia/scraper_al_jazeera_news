b56b4d6dbb3de24f052850b07e7f5c0119fa06f4
Ksenia Yakovleva <xenia.iakovleva@gmail.com>
2019-11-06 11:37:08 +0100

    added comments, removed unnecessary lines

--- get_articles.py
+++ get_articles.py
@@ -1,31 +1,39 @@
+# Scrape articles' texts and titles using Python library Newspaper (from Al Jazeera website, but can be used for others as well)
+
+
 import newspaper
 from newspaper import Article
 import pandas as pd
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 
 driver = webdriver.Firefox()
-df = pd.read_csv('/home/ksenia/Desktop/thesis_scripts/RT_links_merged.csv', sep=',', low_memory=False, na_values = ['no info', '.'])
+#read the csv-file with links to articles
+df = pd.read_csv('/home/ksenia_new/projects/scraping_thesis/al_jazeera_scripts/al_jazeera_links_new.csv', sep=',', low_memory=False, na_values = ['no info', '.'])
 links = list(df['links'])
+urls = []
 headers = []
 texts = []
 for url in links:
 
+		#create an Article object with the url
 		article = Article(url)
 		article.download()
 		article.parse()
+		#print(article.title)
+		#print(article.text)
+		urls.append(url)
+
+		#append article title to the list
 		headers.append(article.title)
+
+		#append article text to the list
 		texts.append(article.text)
+
 		driver.implicitly_wait(5)
 
-final = pd.DataFrame({ 'title' : headers, 'text' : texts})
 
-#final.to_csv('RT_articles_final.csv')
+final = pd.DataFrame({ 'url' : urls, 'title' : headers, 'text' : texts})
 
-
-#(article.html)
-
-#print(articl.text)
+final.to_csv('jazeera_articles_new.csv')
